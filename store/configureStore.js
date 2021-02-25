09c2f73be62843ac2c758d3ee4babd1ad3d21cb3
Nilton <nitinhosilva1996@gmail.com>
2021-02-25 18:52:09 -0300

    organização

---
+++ store/configureStore.js
@@ -0,0 +1,11 @@
+// configureStore.js
+import contador from "./contador.js";
+
+const reducer = Redux.combineReducers({ contador });
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
