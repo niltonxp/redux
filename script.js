9e2c99f88c6ae7f331b65e73c3132945cb95fc1e
Nilton <nitinhosilva1996@gmail.com>
2021-03-11 14:08:06 -0300

    loacalstorage

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-function reducer(state = { loading: false, data: null, error: null }, action) {
+function getLocalStorage(key, initial) {
+  try {
+    return JSON.parse(window.localStorage.getItem(key));
+  } catch (error) {
+    return initial;
+  }
+}
+
+const initialState = {
+  token: getLocalStorage("token", null),
+};
+
+function reducer(state = initialState, action) {
   switch (action.type) {
-    case "FETCH_STARTED":
-      return { ...state, loading: true };
-    case "FETCH_SUCCESS":
-      return { loading: false, data: action.payload, error: null };
-    case "FETCH_ERROR":
-      return { loading: false, error: action.payload, data: null };
+    case "SET_TOKEN":
+      return { token: action.payload };
     default:
       return state;
   }
 }
 
-// thunk
-const thunk = (store) => (next) => (action) => {
-  if (typeof action === "function") {
-    return action(store.dispatch, store.getState);
-  } else {
-    return next(action);
-  }
+// middleware
+const localStorage = (store) => (next) => (action) => {
+  const response = next(action);
+  if (action.localStorage !== undefined)
+    window.localStorage.setItem(
+      action.localStorage,
+      JSON.stringify(action.payload)
+    );
+  return response;
 };
 
 const { compose, applyMiddleware } = Redux;
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
+const enhancer = composeEnhancers(applyMiddleware(localStorage));
 const store = Redux.createStore(reducer, enhancer);
 
-// Action Creator, retorna uma função ao invés de um objeto
-function fetchUrl(url) {
-  return async (dispatch) => {
-    try {
-      dispatch({ type: "FETCH_STARTED" });
-      const data = await fetch(url).then((r) => r.json());
-      dispatch({ type: "FETCH_SUCCESS", payload: data });
-    } catch (error) {
-      dispatch({ type: "FETCH_ERROR", payload: error.message });
-    }
-  };
-}
-
-store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+store.dispatch({
+  type: "SET_TOKEN",
+  payload: "xxxx-xxxx",
+  localStorage: "token",
+});
